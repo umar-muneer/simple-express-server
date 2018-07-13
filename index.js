822ddff8a3946f7e1ecfe72537c4ddfa2cc6b60c
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-07-13 17:11:25 +0500

    adding logs

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ const options = {
   cert: fs.readFileSync(`${credPath}/chained.pem`),
   key: fs.readFileSync(`${credPath}/domain.key`)
 };
+console.log("starting http server");
 http.createServer(app).listen(80);
+console.log("starting https server");
 https.createServer(options, app).listen(443);
