ba62e74ac756707b3e02306ea7dfed980826eac5
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-07-13 17:08:04 +0500

    trying to support https

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
-
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const app = express();
-
 app.get("/", (req, res) => res.json("Reporting"));
 app.get("/health", (req, res) => res.json("Reporting Health"));
-app.listen(80, () => console.log("listening at port 80"));
+
+const credPath = process.env.CRED_FOLDER_PATH;
+const options = {
+  cert: fs.readFileSync(`${credPath}/chained.pem`),
+  key: fs.readFileSync(`${credPath}/domain.key`)
+};
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
