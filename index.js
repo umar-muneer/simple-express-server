08b8c0d3c5148e8c54f43fbc97dd698d2ba0224c
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-07-13 15:26:03 +0500

    added a health route

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.json("Reporting"));
+app.get("/heath", (req, res) => res.json("Reporting Health"));
 app.listen(80, () => console.log("listening at port 80"));
