1e32e5fef5268ad3397395d98ff829cae12924f2
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-07-13 19:54:25 +0500

    basic proxy

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const http = require("http");
 const https = require("https");
+const httpProxy = require("http-proxy");
 const fs = require("fs");
 const path = require("path");
+
 const app = express();
-app.get("/", (req, res) => res.json(`Reporting from ${process.env.SERVER_ID}`));
-app.get("/health", (req, res) => res.json(`Reporting Health ${process.env.SERVER_ID}`));
+const proxy = httpProxy.createServer({});
+
+app.all("*", (req, res) => {
+  const port = req.query.port || 3000;
+  proxy.web(req, res, { target: `localhost:${port}`})
+});
 
 const credPath = process.env.CRED_FOLDER_PATH;
 const options = {
@@ -13,6 +19,6 @@ const options = {
   key: fs.readFileSync(path.join(credPath, "domain.key"))
 };
 console.log("starting http server");
-http.createServer(app).listen(80);
+http.createServer(app).listen(3000);
 console.log("starting https server");
 https.createServer(options, app).listen(443);
