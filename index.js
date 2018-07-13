d0912650138555a84f1cbe0a2757e65bcebc5e9c
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-07-13 19:05:32 +0500

    formatted cred folder path

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
+const path = require("path");
 const app = express();
 app.get("/", (req, res) => res.json("Reporting"));
 app.get("/health", (req, res) => res.json("Reporting Health"));
 
 const credPath = process.env.CRED_FOLDER_PATH;
 const options = {
-  cert: fs.readFileSync(`${credPath}/chained.pem`),
-  key: fs.readFileSync(`${credPath}/domain.key`)
+  cert: fs.readFileSync(path.join(credPath, 'chained.pem')),
+  key: fs.readFileSync(path.join(credPath, 'domain.key'))
 };
 console.log("starting http server");
 http.createServer(app).listen(80);
