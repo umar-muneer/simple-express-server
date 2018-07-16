e05a0a03c0d9eaa2ddecf55073b72e0e065a3222
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-07-16 12:28:08 +0500

    removing https server code

--- index.js
+++ index.js
@@ -7,12 +7,5 @@ const app = express();
 app.get("/", (req, res) => res.json(`Reporting from ${process.env.SERVER_ID}`));
 app.get("/health", (req, res) => res.json(`Reporting Health ${process.env.SERVER_ID}`));
 
-const credPath = process.env.CRED_FOLDER_PATH;
-const options = {
-  cert: fs.readFileSync(path.join(credPath, "chained.pem")),
-  key: fs.readFileSync(path.join(credPath, "domain.key"))
-};
 console.log("starting http server");
 http.createServer(app).listen(80);
-console.log("starting https server");
-https.createServer(options, app).listen(443);
